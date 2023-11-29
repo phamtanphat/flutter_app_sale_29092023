41af2b93837db0bb929a146c6b9ea79edd526e4d
phamtanphat <phatdroid94@gmail.com>
2023-11-29 21:00:55 +0700

    setup ui sign in

--- lib/common/app_constant.dart
+++ lib/common/app_constant.dart
@@ -1,3 +1,7 @@
 class AppConstant {
   static const String BASE_URL = "https://serverappsale.onrender.com/";
+
+  // Assets
+  static const String IMAGE_BANNER_ASSETS = "assets/images/ic_food_express.png";
+  static const String SPLASH_ANIMATION_ASSETS = "assets/animations/animation_splash.json";
 }
--- lib/common/widget/loading_widget.dart
+++ lib/common/widget/loading_widget.dart
@@ -4,37 +4,34 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 
 class LoadingWidget extends StatelessWidget {
-  final Widget child;
-  final BaseBloc bloc;
+  final BaseBloc? bloc;
 
   LoadingWidget({
-    required this.child,
     required this.bloc,
   });
 
   @override
   Widget build(BuildContext context) {
     return StreamProvider<bool>.value(
-      value: bloc.loadingStream,
+      value: bloc?.loadingStream,
       initialData: false,
       child: Stack(
         children: <Widget>[
-          child,
           Consumer<bool>(
             builder: (context, isLoading, child) => Center(
               child: isLoading
                   ? IgnorePointer(
-                ignoring: false,
-                child: Container(
-                  constraints: BoxConstraints.expand(),
-                  decoration: const BoxDecoration(
-                    color: Colors.black45,
-                  ),
-                  child: const SpinKitPouringHourGlass(
-                    color: Colors.white,
-                  ),
-                ),
-              )
+                      ignoring: false,
+                      child: Container(
+                        constraints: BoxConstraints.expand(),
+                        decoration: const BoxDecoration(
+                          color: Colors.black45,
+                        ),
+                        child: const SpinKitPouringHourGlass(
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
                   : Container(),
             ),
           ),
